ca5cea6e510c6bd504eeaba78d4fa09632887714
Gustavo-F <fgustavo016@gmail.com>
2022-01-25 19:44:24 -0300

    iniciada implementaçao de sockets

--- cliente.py
+++ cliente.py
@@ -1,4 +1,6 @@
 import json
+import socket
+import random
 import requests
 
 
@@ -11,15 +13,41 @@ operacao = {
 }
 '''
 
-while True: 
-    print('1 - Débito')
-    print('2 - Crédito')
 
-    escolha = input('Informe o número da operação desejada: ')
+def requisita_transacao(transacao):
+    print(transacao)
+
+
+def main():
+    saldo = 0.0
+    transacao = {}
+
+    socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_cliente.bind(('127.0.0.1', random.randint(1000, 2000)))
+
+    while True: 
+        print('-------------------------------------------------')
+        print(f'Saldo: R$ {saldo}\n')
+
+        print('1 - Débito')
+        print('2 - Crédito')
+
+        escolha = input('Informe o número da operação desejada: ')
+        valor = input('Informe o valor: ')
+
+        try:
+            valor = float(valor)
+
+            if escolha == '1':
+                saldo += valor
+            elif escolha == '2':
+                saldo -= valor
+            else:
+                print('Opção não disponível, tente novamente.')
+        except:
+            print('Valor inválido!')
     
-    if escolha == '1':
-        print('Débito')
-    elif escolha == '2':
-        print('Crédito')
-    else:
-        print('Opção não disponível, tente novamente.')
+        print('\n')
+
+if __name__ == '__main__':
+    main()
--- replica.py
+++ replica.py
@@ -1,3 +1,35 @@
 import sys
 import json
 import socket
+import random
+
+
+PORTA_PRINCIPAL = 6060
+traidor = False
+
+
+def main():
+    argumentos = sys.argv
+    print(argumentos)
+
+    socket_replica = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    if 'traira' in argumentos:
+        traidor = True
+        socket_replica.bind(('127.0.0.1', PORTA_PRINCIPAL))
+    else:
+        socket_replica.bind(('127.0.0.1', random.randint(2000, 3000)))
+
+    socket_replica.listen()
+    
+    socket_cliente, endereco = socket_replica.accept()
+    mensagem = socket_cliente.recv(1024)
+
+    while True:
+        print(f'Cliente conectado => {endereco[0]}:{endereco[1]}')
+
+        socket_cliente, endereco = socket_replica.accept()
+        mensagem = socket_cliente.recv(1024)
+
+if __name__ == '__main__':
+    main()
