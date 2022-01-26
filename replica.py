566964654e46230d09b9dbc4c6945c543377d2dc
Daniel Wzoreck <wzoreck2000@gmail.com>
2022-01-26 11:07:58 -0300

    Funcoes para notificacao e conhecimento de replicas adicionado

--- replica.py
+++ replica.py
@@ -13,6 +13,32 @@ replicas = []
 def validar_operacao(operacao):
     return True
 
+def notifica_criacao_replica(porta):
+    socket_notificacao = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    destino = ('127.0.0.1', settings.PORTA_PRINCIPAL)
+    mensagem = json.dumps({'nova_replica': porta})
+    try:
+        socket_notificacao.connect(destino)
+        socket_notificacao.send(mensagem.encode('UTF-8'))
+        socket_notificacao.close()
+    except:
+        print('Erro ao notificar criacao de réplica!')
+
+def envia_replicas():
+    mensagem = json.dumps({'replicas': replicas})
+    
+    for replica in replicas:
+        socket_notificacao = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        destino = ('127.0.0.1', replica)
+        
+        try:
+            socket_notificacao.connect(destino)
+            socket_notificacao.send(mensagem.encode('UTF-8'))
+        except:
+            print('Erro ao notificar réplicas!')
+        
+        socket_notificacao.close()
+            
 
 def main():
     argumentos = sys.argv
@@ -21,7 +47,9 @@ def main():
     if 'principal' in argumentos:
         socket_replica.bind(('127.0.0.1', settings.PORTA_PRINCIPAL))
     else:
-        socket_replica.bind(('127.0.0.1', random.randint(2000, 3000)))
+        porta = random.randint(2000, 3000)
+        socket_replica.bind(('127.0.0.1', porta))
+        notifica_criacao_replica(porta)
 
     if 'traidor' in argumentos:
         traidor = True
@@ -40,6 +68,14 @@ def main():
         try:
             mensagem = mensagem.decode('UTF-8')
             mensagem = json.loads(mensagem)
+
+            if 'nova_replica' in mensagem:
+                print(f'Nova réplica: {mensagem["nova_replica"]}')
+                replicas.append(mensagem["nova_replica"])
+                envia_replicas()
+            
+            if 'replicas' in mensagem:
+                print(f'Replicas recebidas: {mensagem["replicas"]}')
 
             validar_operacao(mensagem)
         except:
